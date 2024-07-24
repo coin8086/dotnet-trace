8d4bb51baac2b7e5e40daa77d1860a2107e45145
Robert Zhang <louirobert@gmail.com>
2024-07-24 17:33:12 +0800

    Add CustomTraceListener and add it to TraceListeners/App.config

---
+++ CustomTraceListener/CustomTraceListener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CustomTraceListener
+{
+    public class CustomTraceListener : TraceListener
+    {
+        public override void Write(string message)
+        {
+            Console.WriteLine($"CustomTraceListener::Write \"{message}\"");
+        }
+
+        public override void WriteLine(string message)
+        {
+            Console.WriteLine($"CustomTraceListener::WriteLine \"{message}\"");
+        }
+    }
+}
