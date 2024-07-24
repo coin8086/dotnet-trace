961f9f69302de25683e54a7e91729213df82a97a
Robert Zhang <louirobert@gmail.com>
2024-07-24 16:02:00 +0800

    Add TraceListeners

---
+++ TraceListeners/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TraceListeners
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main");
+            Trace.WriteLine("Something happened.");
+            Trace.TraceInformation("Some information is.");
+        }
+    }
+}
