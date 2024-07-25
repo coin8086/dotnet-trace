78cc7fd9785295e498f7a6f57d34f10541d31999
Robert Zhang <louirobert@gmail.com>
2024-07-25 14:34:21 +0800

    Override TraceEvent methods in CustomTraceListener

--- CustomTraceListener/CustomTraceListener.cs
+++ CustomTraceListener/CustomTraceListener.cs
@@ -5,6 +5,8 @@ namespace CustomTraceListener
 {
     public class CustomTraceListener : TraceListener
     {
+        public override bool IsThreadSafe => true;
+
         public override void Write(string message)
         {
             Console.Write($"[\"{message}\"]");
@@ -13,6 +15,21 @@ namespace CustomTraceListener
         public override void WriteLine(string message)
         {
             Console.WriteLine($"[\"{message}\"]\\n");
+        }
+
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id)
+        {
+            TraceEvent(eventCache, source, eventType, id, string.Empty);
+        }
+
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string message)
+        {
+            TraceEvent(eventCache, source, eventType, id, message, new string[0]);
+        }
+
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string format, params object[] args)
+        {
+            Console.WriteLine($"[[{eventCache.DateTime}: {source}-{eventType}-{id}-{string.Format(format, args)}]]");
         }
     }
 }
